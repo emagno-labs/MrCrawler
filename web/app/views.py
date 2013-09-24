d5f74396bea9aaf4ebe4ea77e91f100e35c0434b
eryckson <eryckson@gmail.com>
2013-09-24 00:46:38 -0300

    Formulário de validação de registro

--- web/app/views.py
+++ web/app/views.py
@@ -5,6 +5,7 @@ from wtforms import Form, BooleanField, TextField, PasswordField, validators
 from wtforms.validators import ValidationError
 from core.data.orm.database import db_session
 from core.data.orm.models import User
+import re
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
@@ -44,14 +45,17 @@ def do_crawl():
 def login():
    error = None
    if request.method == 'POST':
-      if request.form['username'] != app.config['USERNAME']:
-         error = 'Credenciais inválidas'
-      elif request.form['password'] != app.config['PASSWORD']:
+      login = request.form['username']
+      password = request.form[ 'password']
+
+      u = User.query.filter(User.login == login).filter(User.pwd == password).first()
+      if u is None:
          error = 'Credenciais inválidas'
       else:
          session['logged_in'] = True
          flash('Você está autenticado :)')
          return redirect(url_for('do_crawl'))
+
    return render_template('login.html', error=error)
 
 @app.route('/logout')
@@ -87,16 +91,39 @@ def favicon():
 def page_not_found(e):
    return render_template('404.html'), 404
 
-#FIXME extrair estas informações para módulos próprios
+# FIXME extrair estas informações para módulos próprios
 # exemplo de validação customizada no WTForms...
 def isEmail(form, field):
-   # FIXME implementar validação de email
-   if field.data != "eryckson@gmail.com":
-      raise ValidationError("O e-mail deve ser 'eryckson@gmail.com'")
+   if not re.match(r"^[a-zA-Z0-9._%-]+@[a-zA-Z0-9._%-]+.[a-zA-Z]{2,6}$", field.data):
+      raise ValidationError("O e-mail informado é inválido.")
+
+   u = User.query.filter(User.email == field.data).first()
+   if u is not None:
+      raise ValidationError('O e-mail informado já está em uso.')
+
+def isStrongPassword(form, field):
+   login = field.data
+   if len(re.findall(r"[A-Z]", login)) <= 0:
+      raise ValidationError('A senha deve possuir ao menos uma letra maiúscula.')
+
+   if len(re.findall(r'[a-z]+', login)) <= 0:
+      raise ValidationError('A senha deve possuir ao menos uma letra minúscula.')
+
+   if len(re.findall(r'\d+', login)) <= 0:
+      raise ValidationError('A senha deve possuir ao menos um número.')
+
+def isUnusedLogin(form, field):
+   login = field.data
+
+   u = User.query.filter(User.login == login).first()
+   if u is not None:
+      raise ValidationError('O login escolhido já está em uso')
 
 class RegistrationForm(Form):
-   # TODO implementar mais validações, como disponibilidade de login
-   login = TextField('Login', [validators.Length(min=4, max=25, message=('O tamanho deve ser entre %(min)d e %(max)d caracteres.'))])
+   login = TextField('Login', [
+      validators.Length(min=4, max=25, message=('O tamanho deve ser entre %(min)d e %(max)d caracteres.')),
+      isUnusedLogin
+   ])
    name = TextField('Nome', [validators.Length(min=4, max=50, message=('O tamanho deve ser entre %(min)d e %(max)d caracteres.'))]) 
    email = TextField('Email', [
       validators.Length(min=6, max=35, message=('O tamanho deve ser entre %(min)d e %(max)d caracteres.')),
@@ -104,7 +131,7 @@ class RegistrationForm(Form):
    ])
    password = PasswordField('Senha', [
       validators.Required(message=('Este campo é obrigatório')),
-      validators.EqualTo('confirm', message='As senhas devem ser iguais')
+      validators.EqualTo('confirm', message='As senhas devem ser iguais'),
+      isStrongPassword
    ])
    confirm = PasswordField('Repita a senha')
-
